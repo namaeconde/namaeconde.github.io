cf82e22ba37b883c6dba8bc3ca44811a54e7eee3
Namae Conde <lunamae.conde@sensis.com.au>
2021-09-25 21:15:41 +1000

    Apply material ui dark mode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+import CssBaseline from '@mui/material/CssBaseline';
 import PortfolioPage from "./PortfolioPage";
 
 export default function App() {
   return (
-    <PortfolioPage />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <PortfolioPage />
+    </ThemeProvider>
   );
 }
--- src/theme.js
+++ src/theme.js
@@ -1,19 +1,25 @@
+import { createTheme } from '@mui/material/styles';
+
 export const color = {
   tangaroa: {
-    900: "#1E2F3C",
-    800: "#4B5963",
-    700: "#6F7A82",
-    600: "#8C959B",
-    500: "#A3AAAF",
-    400: "#B5BBBF",
-    300: "#C4C9CC",
-    200: "#D0D4D6",
-    100: "#D9DDDE",
-    50: "#E1E4E5"
+    900: "#001E3C",
+    800: "#334B63",
+    700: "#5C6F82",
+    600: "#7D8C9B",
+    500: "#97A3AF",
+    400: "#ACB5BF",
+    300: "#BDC4CC",
+    200: "#CAD0D6",
+    100: "#D5D9DE",
+    50: "#DDE1E5"
   },
   white: "#FFFFFF"
 };
 
-const theme = {};
+const theme = createTheme({
+  palette: {
+    mode: "dark"
+  }
+});
 
 export default theme;
