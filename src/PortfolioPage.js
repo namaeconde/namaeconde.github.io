6e709a3327f0272c40bddca401756ecf22ce1143
Namae Conde <lunamae.conde@sensis.com.au>
2021-10-01 22:20:41 +0800

    Add business card layout

--- src/PortfolioPage.js
+++ src/PortfolioPage.js
@@ -1,10 +1,46 @@
-import { Box } from '@mui/material';
+import {
+  Box,
+  Stack,
+  Typography,
+  Card,
+  CardActions,
+  CardContent,
+  IconButton
+} from '@mui/material';
+import {
+  Email as EmailIcon,
+  LinkedIn as LinkedInIcon,
+  GitHub as GitHubIcon,
+  LocalPhone as PhoneIcon
+} from '@mui/icons-material';
+
 import { color } from './theme.js';
 
 export const ParticlesCanvas = () => {
   return (
-    <canvas className="background" />
+    <canvas className="background" style={{ position: "absolute" }}/>
   )
+}
+
+const BusinessCard = () => {
+  return (
+    <Card sx={{ width: 275 }} style={{ zIndex: 1 }}>
+      <CardContent>
+        <Typography variant="body1" color={ color.atlantis[900] }>
+          Lunamae Shalom M. Conde
+        </Typography>
+        <Typography variant="caption" color="text.secondary">
+          Software Engineer
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <IconButton href="tel:+6399992216589"><PhoneIcon /></IconButton>
+        <IconButton href="mailto:namaeconde@gmail.com"><EmailIcon /></IconButton>
+        <IconButton href="https://www.linkedin.com/in/namaeconde" target="_blank"><LinkedInIcon /></IconButton>
+        <IconButton href="https://github.com/namaeconde" target="_blank"><GitHubIcon /></IconButton>
+      </CardActions>
+    </Card>
+  );
 }
 
 export default function PortfolioPage() {
@@ -15,6 +51,9 @@ export default function PortfolioPage() {
       minHeight="100vh"
       bgcolor={ color.tangaroa[900] }
     >
+      <Stack spacing={2} margin="0 auto" justifyContent="center">
+        <BusinessCard />
+      </Stack>
       <ParticlesCanvas />
     </Box>
   )
--- src/theme.js
+++ src/theme.js
@@ -13,12 +13,30 @@ export const color = {
     100: "#D5D9DE",
     50: "#DDE1E5"
   },
+  atlantis: {
+    900: "#A6E22E",
+    800: "#B8E858",
+    700: "#C6ED79",
+    600: "#D1F194",
+    500: "#DAF4A9",
+    400: "#E1F6BA",
+    300: "#E7F8C8",
+    200: "#ECF9D3",
+    100: "#F0FADC",
+    50: "#F3FBE3"
+  },
+  black: {
+    900: "#27282c"
+  },
   white: "#FFFFFF"
 };
 
 const theme = createTheme({
   palette: {
     mode: "dark"
+  },
+  typography: {
+    "fontFamily": `"Consolas", "Liberation Mono", "Menlo", "Courier", "monospace"`
   }
 });
 
