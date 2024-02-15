e8dd70dbdae905570b52b50170901e2d48feb51f
Namae Conde <namaeconde@gmail.com>
2024-02-15 20:16:49 +1100

    Update astro config to github domain

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,7 @@ import { expressiveCodeOptions } from "./src/site.config";
 // https://astro.build/config
 export default defineConfig({
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://my-astro-website-kappa.vercel.app/",
+	site: "https://namaeconde.github.io/",
 	markdown: {
 		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
 		rehypePlugins: [
